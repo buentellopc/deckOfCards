c7993a8a5bb852dfacb09fd4ce4fc28234070373
Buentello <buentellopc@gmail.com>
2021-07-25 14:31:49 -0500

    display a new card when button is clicked

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./Deck.css";
 
 type DeckProps = {};
 type DeckState = {
   deckId: string;
-  cards: object[];
+  cards: { cards: { image: string }[] }[];
 };
 
 class Deck extends Component<DeckProps, DeckState> {
@@ -18,19 +19,32 @@ class Deck extends Component<DeckProps, DeckState> {
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {}
+  async handleClick() {
+    if (this.state.deckId) {
+      const card = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${this.state.deckId}/draw/`
+      );
+      console.log(card);
+
+      this.setState((st) => ({ cards: [...st.cards, card.data] }));
+    }
+  }
 
   async componentDidMount() {
     const response = await axios.get(
       "https://deckofcardsapi.com/api/deck/new/shuffle"
     );
-    console.log(response);
+    // console.log(response);
+    this.setState({ deckId: response.data.deck_id });
   }
   render() {
     return (
       <div>
         <h1>Deck card component</h1>
         <button onClick={this.handleClick}>New card</button>
+        {this.state.cards.map((data, i) => (
+          <img key={i} src={data.cards[0].image} />
+        ))}
       </div>
     );
   }
